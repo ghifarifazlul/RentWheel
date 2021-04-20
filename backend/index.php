37f00d2fffd46e3af30d079e97bd301b8ee4fcd7
ghifari fazlul <45381123+ghifarifazlul@users.noreply.github.com>
2021-04-20 12:18:38 +0800

    Delete ~$PL RentWheel.docx

---
+++ backend/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Membuat Form Validasi dengan Javascript - WWW.MALASNGODING.COM</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+<body>
+	<br/>
+	<br/>
+	<center><h2>WWW.MALASNGODING.COM</h2></center>	
+	<br/>
+	<div class="login">
+	<br/>
+		<form action="login.php" method="post" onSubmit="return validasi()">
+			<div>
+				<label>Username:</label>
+				<input type="text" name="username" id="username" />
+			</div>
+			<div>
+				<label>Password:</label>
+				<input type="password" name="password" id="password" />
+			</div>			
+			<div>
+				<input type="submit" value="Login" class="tombol">
+			</div>
+		</form>
+	</div>
+</body>
+
+<script type="text/javascript">
+	function validasi() {
+		var username = document.getElementById("username").value;
+		var password = document.getElementById("password").value;		
+		if (username != "" && password!="") {
+			return true;
+		}else{
+			alert('Username dan Password harus di isi !');
+			return false;
+		}
+	}
+
+</script>
+</html>
